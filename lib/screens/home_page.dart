b5e77a593d1b4ff48178342489fcf061ebf65216
HasanAli <h.alisiseci@gmail.com>
2020-11-02 22:50:57 +0300

    last version

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -9,144 +9,121 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   DatabaseHelper _databaseHelper = DatabaseHelper();
-  List<Notes> allNotes = List<Notes>();
+  List<Notes> allNotes = new List<Notes>();
   bool aktiflik = false;
   var _controllerTitle = TextEditingController();
   var _controllerDesc = TextEditingController();
   var _formKey = GlobalKey<FormState>();
-  int clickedNoteIndex;
   int clickedNoteID;
+
+  void getNotes() async {
+    var notesFuture = _databaseHelper.getAllNotes();
+    await notesFuture.then((data) {
+      setState(() {
+        this.allNotes = data;
+      });
+    });
+  }
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
-    _databaseHelper.allNotes().then((allNotesMapList) {
-      for (Map noteInMapList in allNotesMapList) {
-        allNotes.add(Notes.fromMap(noteInMapList));
-      }
-      setState(() {});
-    }).catchError((error) => print("Hata:" + error));
+    getNotes();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Notlarım"),
-      ),
-      body: Container(
-        child: Column(
-          children: <Widget>[
-            Form(
+        appBar: AppBar(
+          title: Text("Notlarım"),
+        ),
+        body: Container(
+            child: Column(children: <Widget>[
+          Form(
               key: _formKey,
-              child: Column(
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: TextFormField(
-                      autofocus: false,
-                      controller: _controllerTitle,
-                      decoration: InputDecoration(
-                        labelText: "Başlık",
-                        border: OutlineInputBorder(),
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: TextFormField(
-                      autofocus: false,
-                      controller: _controllerDesc,
-                      decoration: InputDecoration(
-                        labelText: "Açıklama",
-                        border: OutlineInputBorder(),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Row(
+              child: Column(children: <Widget>[
+                buildForm(_controllerTitle, "Başlık"),
+                buildForm(_controllerDesc, "Açıklama")
+              ])),
+          Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: <Widget>[
-                RaisedButton(
-                  child: Text("Kaydet"),
-                  color: Colors.green,
-                  onPressed: () {
-                    if (_formKey.currentState.validate()) {
-                      _addNote(
-                          Notes(_controllerTitle.text, _controllerDesc.text));
-                    }
-                  },
-                ),
-                RaisedButton(
-                  child: Text("Güncelle"),
-                  color: Colors.yellow,
-                  onPressed: clickedNoteID == null
-                      ? null
-                      : () {
-                          if (_formKey.currentState.validate()) {
-                            _uptadeNote(Notes.withID(clickedNoteID,
-                                _controllerTitle.text, _controllerDesc.text));
-                          }
-                        },
-                ),
-              ],
-            ),
-            Expanded(
+                buildButton("Kaydet"),
+                buildButton("Güncelle")
+              ]),
+          Expanded(
               child: ListView.builder(
-                itemCount: allNotes.length,
-                itemBuilder: (context, index) {
-                  return Card(
-                    child: ListTile(
-                      onTap: () {
-                        setState(() {
-                          _controllerTitle.text = allNotes[index].title;
-                          _controllerDesc.text = allNotes[index].description;
-                          clickedNoteIndex = index;
-                          clickedNoteID = allNotes[index].id;
-                        });
-                      },
-                      title: Text(allNotes[index].title),
-                      subtitle: Text(allNotes[index].description),
-                      trailing: GestureDetector(
-                        onTap: () {
-                          _deleteNote(allNotes[index].id, index);
-                        },
-                        child: Icon(Icons.delete),
-                      ),
-                    ),
-                  );
-                },
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
+                  itemCount: allNotes.length,
+                  itemBuilder: (context, index) {
+                    return Card(
+                        child: ListTile(
+                            onTap: () {
+                              setState(() {
+                                _controllerTitle.text = allNotes[index].title;
+                                _controllerDesc.text =
+                                    allNotes[index].description;
+                                clickedNoteID = allNotes[index].id;
+                              });
+                            },
+                            title: Text(allNotes[index].title),
+                            subtitle: Text(allNotes[index].description),
+                            trailing: GestureDetector(
+                              onTap: () {
+                                _deleteNote(allNotes[index].id, index);
+                              },
+                              child: Icon(Icons.delete),
+                            )));
+                  }))
+        ])));
+  }
+
+  Widget buildForm(TextEditingController txtController, String str) {
+    return Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: TextFormField(
+            autofocus: false,
+            controller: txtController,
+            decoration:
+                InputDecoration(labelText: str, border: OutlineInputBorder())));
+  }
+
+  Widget buildButton(String str) {
+    return RaisedButton(
+        child: Text(str),
+        color: str == "Kaydet" ? Colors.green : Colors.yellow,
+        onPressed: str == "Kaydet"
+            ? () {
+                if (_formKey.currentState.validate()) {
+                  _addNote(Notes(_controllerTitle.text, _controllerDesc.text));
+                }
+              }
+            : clickedNoteID == null
+                ? null
+                : () {
+                    if (_formKey.currentState.validate()) {
+                      _uptadeNote(Notes.withID(clickedNoteID,
+                          _controllerTitle.text, _controllerDesc.text));
+                    }
+                  });
   }
 
   //Crud İşlemlerinin AraYüze Uygulanması
 
   void _addNote(Notes note) async {
-    var addedNoteID = await _databaseHelper.addNote(note);
-    note.id = addedNoteID;
+    await _databaseHelper.insert(note);
 
-    if (addedNoteID > 0) {
-      setState(() {
-        allNotes.insert(0, note);
-        _controllerTitle.text = "";
-        _controllerDesc.text = "";
-      });
-    }
+    setState(() {
+      getNotes();
+      _controllerTitle.text = "";
+      _controllerDesc.text = "";
+    });
   }
 
   void _uptadeNote(Notes note) async {
-    var sonuc = await _databaseHelper.updateNote(note);
+    await _databaseHelper.update(note);
 
     setState(() {
-      allNotes[clickedNoteIndex] = note;
+      getNotes();
       _controllerTitle.text = "";
       _controllerDesc.text = "";
       clickedNoteID = null;
@@ -154,10 +131,10 @@ class _HomePageState extends State<HomePage> {
   }
 
   void _deleteNote(int deletedNoteId, int deletedNoteIndex) async {
-    var sonuc = await _databaseHelper.deleteNote(deletedNoteId);
+    await _databaseHelper.delete(deletedNoteId);
 
     setState(() {
-      allNotes.removeAt(deletedNoteIndex);
+      getNotes();
     });
   }
 }
--- lib/utils/dbHelper.dart
+++ lib/utils/dbHelper.dart
@@ -3,7 +3,6 @@ import 'package:sqflite/sqflite.dart';
 import 'package:sqflite_demo/models/notes.dart';
 
 class DatabaseHelper {
-  static DatabaseHelper _databaseHelper;
   static Database _database;
 
   String _notesTable = "notes";
@@ -11,74 +10,49 @@ class DatabaseHelper {
   String _columnTitle = "title";
   String _columnDescription = "description";
 
-  //Kontroller sonrası değerler aktaracağımız için "factory" anahtar kelimesini kullandık.
-  factory DatabaseHelper() {
-    if (_databaseHelper == null) {
-      _databaseHelper = DatabaseHelper._internal();
-      return _databaseHelper;
-    } else {
-      return _databaseHelper;
-    }
-  }
-  //Singleton sınıfların dışarıdan nesnesini oluşmasını istemediğimiz
-  //için Constructor’ları private olarak belirtilmelidir.
-  DatabaseHelper._internal();
-
-  Future<Database> _getDatabase() async {
+  Future<Database> get database async {
     if (_database == null) {
-      _database = await _initializeDatabase();
-      return _database;
-    } else {
-      return _database;
+      _database = await initializeDatabase();
     }
+    return _database;
   }
 
-  _initializeDatabase() async {
+  Future<Database> initializeDatabase() async {
     String dbPath = join(await getDatabasesPath(), "notes.db");
-    var notesDb = openDatabase(dbPath, version: 1, onCreate: _createDb);
+    var notesDb = await openDatabase(dbPath, version: 1, onCreate: createDb);
     return notesDb;
   }
 
-  Future<void> _createDb(Database db, int version) async {
+  void createDb(Database db, int version) async {
     await db.execute(
-        "CREATE TABLE $_notesTable ($_columnID INTEGER PRIMARY KEY AUTOINCREMENT, $_columnTitle TEXT, $_columnDescription TEXT )");
+        "Create table $_notesTable($_columnID integer primary key, $_columnTitle text, $_columnDescription text)");
   }
 
   //Crud Methods
-  Future<List<Map<String, dynamic>>> allNotes() async {
-    var db = await _getDatabase();
-    var result = await db.query(_notesTable, orderBy: "$_columnID DESC");
+  Future<List<Notes>> getAllNotes() async {
+    Database db = await this.database;
+    var result = await db.query("$_notesTable");
+    return List.generate(result.length, (i) {
+      return Notes.fromMap(result[i]);
+    });
+  }
+
+  Future<int> insert(Notes note) async {
+    Database db = await this.database;
+    var result = await db.insert("$_notesTable", note.toMap());
     return result;
   }
 
-  Future<int> addNote(Notes note) async {
-    //değişken oluşturup, var olan database'imizi çağırıp oluşturduğumuz değişkene atıyoruz
-    var db = await _getDatabase();
-    //daha sonrasında db üzerinden insert methodunu kullanarak ekleme işlemini yapıyoruz.
-    //insert methodu bizden gelen verinin ekleneceği tabloyu, eklenecek veriyi(burda map'e çeviriyoruz)
-    //ve nullColumnHack değeri istiyor, biz columnID değerini atıyoruz.
-    var result = await db.insert(_notesTable, note.toMap(),
-        nullColumnHack: "$_columnID");
+  Future<int> delete(int id) async {
+    Database db = await this.database;
+    var result = await db.rawDelete("delete from $_notesTable where id=$id");
     return result;
   }
 
-  //Not güncelleme işleminde de ilk olarak not ekleme işlemindeki gibi database'i alıyoruz.
-  Future<int> updateNote(Notes note) async {
-    var db = await _getDatabase();
-    //Daha sonra update metoduyla gelen notu güncelleme işlemine gönderiyoruz burada where ve whereArgs
-    //parametreleri isteniyoruz bizden. Where için columnId değerini
-    var result = await db.update(_notesTable, note.toMap(),
-        where: "$_columnID = ?", whereArgs: [note.id]);
-    return result;
-  }
-
-  //Not silme işlemimiz de ise parametre olarak sadece id göndermemiz işimizi görecektir.
-  Future<int> deleteNote(int id) async {
-    //Not silme işleminde de ilk olarak not ekleme işlemindeki gibi database'i alıyoruz.
-    var db = await _getDatabase();
-    //Daha sonra database'den ilgili id'deki değerleri delete etmesini istiyoruz.
-    var result =
-        await db.delete(_notesTable, where: "$_columnID", whereArgs: [id]);
+  Future<int> update(Notes note) async {
+    Database db = await this.database;
+    var result = await db.update("$_notesTable", note.toMap(),
+        where: "id=?", whereArgs: [note.id]);
     return result;
   }
 }
